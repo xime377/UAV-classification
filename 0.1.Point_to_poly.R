69abb390ec1dafe8b9054572727eada6e2f56a33
Ximena Tagle Casapía <xtagle@iiap.org.pe>
2019-11-25 11:48:43 +0100

    Script

--- 0.1.Point_to_poly.R
+++ 0.1.Point_to_poly.R
@@ -2,73 +2,70 @@
 #
 #
 #This script generates a polygon shapefile from coordinates points
-#Any questions or improvements can be emailed to Ximena Tagle: xtagle@iiap.org.pe
+#Any questions or improvements can be emailed to Ximena Tagle: xtagle@iiap.gob.pe
 #
 #
 
+###SET WORKING DIRECTORY
+setwd("G:/My Drive/Monan/RPAs/Misiones/")
 
-###SET Working Directory 
-
-#setwd("/home/xtagle/UAV/Results/") #Linux
-setwd("G:/My Drive/Monan/Taller_Oct/") #Windows
-
-
-#Load libraries
+###LOAD LIBRARIES
 library("sf")
 library("mapview")
 library("RColorBrewer")
 library(raster)
 library(RCurl)
 
-#load points
+
+###LOAD POINTS
 dir(,"csv")
-Plots<-read.csv("./1_Clip/IIAP_v.csv", header=T)
+Plots<-read.csv("./GCPs/Puntos RTK.csv", header=T)
 head(Plots)
 str(Plots)
 
-#Load function
-source("./lib/Point_to_polygon.R")
-#or 
+###LOAD FUNCTION
+#Local dir
+source("./Results/lib/Point_to_polygon.R")
+##or github
 #script <- getURL("https://raw.githubusercontent.com/xime377/UAV-classification/master/Point_to_polygon.R", ssl.verifypeer = F)
 #eval(parse(text = script))
 
-
+###CREATE A SF 
 #Convert to Sp <- st_as_sf(pnt, coords = c("x", "y"), crs = 4326)
 #RPlots<- st_as_sf(Plots, coords = c("Lon", "Lat"), crs = 4326) #if they are in DD
 RPlots<- st_as_sf(Plots, coords = c("X", "Y"), crs = 32718) #if they are in UTM
 head(RPlots) 
 
-#Plotting points
+###PLOTTING POINTS
 plot(RPlots[,1])
 
 mapView(RPlots, zcol = "Plot", color= "white",legend=T, 
         map.type=c("Stamen.Terrain", "CartoDB.Positron", "Esri.WorldImagery", "OpenStreetMap", "OpenTopoMap"))
 
 
-#Export as shapefile
+####Export as a shapefile
 R.plots<-st_as_sf(RPlots)
-st_write(R.plots, "./1_Clip/IIAP_v.shp")
+st_write(R.plots, "./ALP/ALP06.shp")
 
 
 
-###Points to polygon
-
-#Load a shp (if it is not loaded yet)
-Rplots<-shapefile("./1_Clip/IIAP_v.shp")
+###POINTS TO POLYGON
+#Load a shp
+Rplots<-shapefile("./ALP/ALP-60.shp")
 Rplots@coords
 
 
-#With max and min (when only a rectangle is needed)
+#With max and min
 # ext <- as(raster::extent(78.46801, 78.83157, 19.53407, 19.74557), "SpatialPolygons")
 # proj4string(e) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 # plot(ext)
 
 
 #Point to poly
-IIAP<-Point.to.poly(Rplots)
-plot(IIAP)
+ALP60<-Point.to.poly(Rplots)
+plot(ALP60)
 
 #Export as shapefile
-IIAP<-st_as_sf(IIAP)
-st_write(IIAP, "./1_Clip/IIAP_poly.shp")
+ALP60<-st_as_sf(ALP60)
+st_write(ALP60, "./ALP/ALP60_poly.shp")
 
