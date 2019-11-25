d252e0c67380e2ab85936ee18916b82bb82f0d4d
Ximena Tagle Casapía <xtagle@iiap.org.pe>
2019-11-25 11:43:11 +0100

    lib

--- Point_to_polygon.R
+++ Point_to_polygon.R
@@ -2,7 +2,7 @@
 #'
 #' @param pointsP Data frame with Vertex, Latitude, Longitude and Altitude (From ForestPlots)
 #'
-#' @return sf Polygon 
+##' @return sf Polygon 
 #'
 
 Point.to.poly<-function(pointsP){
